eeaebd6a3725fe9776ce6224b8c18cf5d3f04b5c
Nathan <nathanbrentlloyd2000@gmail.com>
2025-07-30 08:15:29 +0000

    Fishing touches on design, finalize JS logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,91 @@
 'use strict';
 
 function main() {
+    let curr = '';
+    let next = '';
     let operator = '';
-    let n = '';
-    let m = '';
 
     const btn = function(b) {
         return document.querySelector(`#${b}`);
     }
 
-    const buttonOne = btn('one');
-    const buttonTwo = btn('two');
-    const buttonThree = btn('three');
-    const buttonFour = btn('four');
-    const buttonFive = btn('five');
-    const buttonSix = btn('six');
-    const buttonSeven = btn('seven');
-    const buttonEight = btn('eight');
-    const buttonNine = btn('nine');
+    const btnDelete = btn('delete');
+    const clear = btn('clear');
+    const btnEquals = btn('equals');
+    const btnDecimal = btn('dot');
+    const btnNegate = btn('negate');
 
-}
+    const btnArray = [];
 
+    const display = document.querySelector('.display');
+    const btns = Array.from(document.querySelectorAll('.number-btn'));
+    const operatorBtns = Array.from(document.querySelectorAll('.operator'));
+    const historyDisplay = document.querySelector('.display-history');
 
-function add(n, m) {
-    return n + m;
-}
-
-function subtract(n, m) {
-    return n - m;
-}
-
-function multiply(n, m) {
-    return n * m;
-}
-
-function divide(n, m) {
-    return n === 0 || m === 0 ? undefined : n / m;
-}
-
-function operate(fn) {
-    return function(n, m) {
-        return fn(n, m);
+    for (const btn of btns) {
+        let color = btn.style.backgroundColor;
+        btn.addEventListener('click', (e) => {
+            if (!operator.length) {
+                curr += btn.textContent;
+                display.textContent = curr;
+            }
+            else {
+                next += btn.textContent;
+                display.textContent = next;
+            }
+        });
+        btn.addEventListener('mouseover',(e) => {
+            e.target.setAttribute('style', 'background-color: gray;');
+            e.target.addEventListener('mouseleave', (e) => {
+                e.target.setAttribute('style', `background-color: ${color};`);
+            });
+        });
     }
+
+    for (const btn of operatorBtns) {
+        btn.addEventListener('click', (e) => {
+            if (!curr) return;
+            if (!operator) {
+                operator = btn.textContent;
+                historyDisplay.textContent = `${curr} ${operator}`;
+            }
+            console.log(operator);
+            if (next) {
+                curr = perform(operator, curr, next);
+                operator = btn.textContent;
+                historyDisplay.textContent = `${curr} ${operator}`;
+                display.textContent = curr;
+                next = '';
+            }
+        });
+    }
+
+    btnDelete.addEventListener('click', (e) => {
+        curr = '';
+        next = '';
+        operator = '';
+        document.querySelector('.display').textContent = '';
+        document.querySelector('.display-history').textContent = '';
+    });
+
+    btnEquals.addEventListener('click', (e) => {
+        curr = perform(operator, curr, next);
+        next = '';
+        operator = '';
+        display.textContent = curr;
+    });
 }
 
-function passCallback(fn) {
-    return operate(fn);
+const add = (n, m) => +n + +m;
+const subtract = (n, m) => +n - +m;
+const multiply = (n, m) => +n * +m;
+const divide = (n, m) => n === 0 || m === 0 ? undefined : +n / +m;
+
+function perform(operator, n, m) {
+    if (operator === '+') return add(n, m);
+    else if (operator === '-') return subtract(n, m);
+    else if (operator === 'x') return multiply(n, m);
+    else if (operator === '/') return divide(n, m);
 }
 
-const addFn = operate(add);
-const subtractFn = operate(subtract);
-const multiplyFn = operate(multiply);
-const divideFn = operate(divide);
-
+main();
